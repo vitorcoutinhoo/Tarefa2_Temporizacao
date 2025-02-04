49e10b2da5ee24be43f1f33b42c255833c70e7ad
Vitor <vitorcooutinho@gmail.com>
2025-02-03 22:08:20 -0300

    feat: implement debouncing in one shot timer arquive

--- Temp_Oneshot/Temp_Oneshot.c
+++ Temp_Oneshot/Temp_Oneshot.c
@@ -2,16 +2,19 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
+// Definição dos pinos
 #define RED_LED 13
 #define BLUE_LED 12
 #define GREEN_LED 11
 #define BUTTON 5  // Botão a
 
-// Variaveis globais
-uint leds[3] = {BLUE_LED, RED_LED, GREEN_LED};
-bool leds_on[3] = {false, false, false};
-uint current = 0;
-bool processing = false;
+
+uint leds[3] = {BLUE_LED, RED_LED, GREEN_LED}; // Ordem a ser desligada
+bool leds_on[3] = {false, false, false}; // Led aceso: true
+
+uint current = 0; // Led a ser desligado
+bool processing = false; // Impede o botão funcionar novamente durante o processo
+
 
 // Função para iniciar um pino e setar direção
 void init_gpio(uint gpio, uint dir) {
@@ -22,13 +25,15 @@ void init_gpio(uint gpio, uint dir) {
 // Função callback para desligar os leds
 uint64_t turn_off_callback(alarm_id_t id, void *user_data) {
     if (current < 3) {
-        gpio_put(leds[current], 0);
+        gpio_put(leds[current], 0); 
         leds_on[current] = false;
 
+        // Passa para o próximo led a ser desligado
         current++;
 
+        // Chama a função de callback novamente, até todos os led estiverem desligados
         if (current < 3) add_alarm_in_ms(3000, turn_off_callback, NULL, false);
-        else processing = false;
+        else processing = false; // Habilida o botão novamente
     }
 
     return 0;
@@ -55,18 +60,27 @@ int main() {
     gpio_pull_up(BUTTON);
 
     while (true) {
-        if (gpio_get(BUTTON) == 0 && all_off() && !processing) {
-            processing = true;
-            current = 0;
+        if (gpio_get(BUTTON) == 0) {
+            sleep_ms(50); // Debouncing simples, espera 50 ms e verifica se o botão continua pressionado
+            
+            if (gpio_get(BUTTON) == 0 && all_off() && !processing) {
+            processing = true; // Trava o botão de funcionar durante esse processo
+            current = 0; 
 
+            // Liga todos os leds
             for (int i = 0; i < 3; i++) {
                 gpio_put(leds[i], 1);
                 leds_on[i] = true;
             }
 
+            // Chama a função callback
             add_alarm_in_ms(3000, turn_off_callback, NULL, false);
-        }   
+        }
+        }
 
+           
+
+        // Sleep pra não sobrecarregar o processamento
         sleep_ms(10);
     }
 }
